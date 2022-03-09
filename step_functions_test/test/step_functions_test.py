7728a7917e34f982af6cd0354877d2285eabb7d8
derwind <uncertainty_principle_ss@yahoo.co.jp>
2022-03-10 02:06:26 +0900

    update a test

--- step_functions_test/test/step_functions_test.py
+++ step_functions_test/test/step_functions_test.py
@@ -3,10 +3,14 @@ import json
 import time
 
 def main():
+    fibonacci = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
     input = {
         'n_terms': 7,
         'exponent': 2
     }
+
+    answer = sum([v ** input['exponent'] for v in fibonacci[:input['n_terms']]])
 
     client = boto3.client('stepfunctions', endpoint_url='http://localhost:4566/')
     response = client.start_execution(input=json.dumps(input), stateMachineArn='arn:aws:states:us-east-1:000000000000:stateMachine:FibonacciStateMachine')
@@ -15,7 +19,9 @@ def main():
     for _ in range(max_try):
         response = client.describe_execution(executionArn=response['executionArn'])
         if response['status'] != 'RUNNING':
-            print(response)
+            output = json.loads(response['output'])
+            value = output['value']
+            print(f'result: {value==answer}')
             break
         else:
             time.sleep(1)
